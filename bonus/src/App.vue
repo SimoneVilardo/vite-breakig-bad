66b249d9c0cd5c4a0988d50588bc8f8cb0019705
SimoneVilardo <simovila95@gmail.com>
2023-06-22 17:13:12 +0200

    [ADD] cartella bonus

---
+++ bonus/src/App.vue
@@ -0,0 +1,39 @@
+
+<script>
+import axios from 'axios';
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+import { store } from './store.js';
+
+export default {
+  components:{
+    AppHeader,
+    AppMain
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+
+  mounted() {
+    axios.get(store.apiUrl).then((response) => {
+      store.pokemonList = response.data.docs;
+    })
+  },
+}
+</script>
+<template>
+  <div>
+    <AppHeader />
+    <AppMain />
+  </div>
+</template>
+<style lang="scss">
+@use './styles/generals.scss'as *;
+@use './styles/partials/variables' as *;
+  
+</style>
