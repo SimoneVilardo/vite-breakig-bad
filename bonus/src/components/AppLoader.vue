7069dcbb2557f1a18e460bbf3112a246c7f557d5
SimoneVilardo <simovila95@gmail.com>
2023-06-22 18:04:34 +0200

    bonus modificato

--- bonus/src/components/AppLoader.vue
+++ bonus/src/components/AppLoader.vue
@@ -1,13 +1,5 @@
 <script>
-import { store } from '../store.js';
 
-export default {
-    data() {
-        return {
-            store
-        }
-    },
-}
 </script>
 <template>
     <div class="my-row">
